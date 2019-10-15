7330de32b7062f3fe2153da2cb3a1f360f9858b7
Ubuntu <vanja.wallner@me.com>
2019-10-15 18:49:19 +0000

    tasks updated

--- tasks.py
+++ tasks.py
@@ -1,14 +1,25 @@
 from celery import Celery
+from flask import Flask, jsonify
 import json
+
+appFlask = Flask(__name__)
 
 app = Celery('tasks', broker='pyamqp://guest@localhost//')
 
-@app.task
-def add(x, y):
-    return x + y
+@appFlask.route('/test', methods=['GET'])
+def a():
+    return 4 
 
-@app.task
-def js():
-    with open('json.txt') as json_file:
-    	data = json.load(json_file)
-    	return data['id']
+#@app.task
+#def add(x, y):
+#    return x + y
+
+#@app.task
+#def readJSON():
+#    with open('json.txt') as json_file:
+#    	data = json.load(json_file)
+#    	return data['id']
+
+if __name__ == '__main__':
+    
+    appFlask.run(host='0.0.0.0',debug=True)

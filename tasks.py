307124c6a8268e74152195e4c85f3c8ff1a186c8
vanjaw <vanja.wallner@me.com>
2019-10-17 14:17:09 +0200

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -6,8 +6,43 @@ appFlask = Flask(__name__)
 
 app = Celery('tasks', broker='pyamqp://guest@localhost//')
 
-@appFlask.route('/test', methods=['GET'])
-def a():
+@appFlask.route('/jsoncount', methods=['GET'])
+def js():
+    data = []
+    with open ('6e72a55b-6b6a-4958-9128-a54f58df58eb', 'r') as f:
+        for line in f:
+            if line not in ['\n', '\r\n']:
+                data.append(json.loads(line))
+    hencount = 0
+    hancount = 0
+    honcount = 0
+    dencount = 0
+    detcount = 0
+    dennecount = 0
+    dennacount = 0
+    count = 0
+
+    for obj in data:
+        if obj["retweeted"] == False:
+            text = obj["text"]
+            text = text.lower()
+            hencount += text.count("hen")
+            hancount += text.count("han")
+            honcount += text.count("hon")
+            dencount += text.count("den")
+            detcount += text.count("det")
+            dennecount += text.count("denne")
+            dennacount += text.count("denna")
+            count += 1
+
+    return("{\"han\": " + str(hancount) +
+           ", \"hon\": " + str(honcount) +
+           ", \"hen\": " + str(hencount) +
+           ", \"den\": " + str(dencount) +
+           ", \"det\": " + str(detcount) +
+           ", \"denne\": " + str(dennecount) +
+           ", \"denna\": " + str(dennacount) +
+           ", \"nrOfTweets\":"  + str(count) + "}")
     return 4 
 
 #@app.task
